211ec632c004378d8db16858c4ce4d8dfffe75ac
Tunahan Danış <tunahandanis@outlook.com>
2022-04-03 17:38:38 +0300

    add shooting logic

--- src/pages/game-modes/easy-mode.js
+++ src/pages/game-modes/easy-mode.js
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 
 const EasyMode = () => {
   const [cursorStyle, setCursorStyle] = useState();
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [isTargetOn, setIsTargetOn] = useState(false);
+  const [targetPosition, setTargetPosition] = useState();
 
   useEffect(() => {
     document.addEventListener("mousemove", (e) => {
@@ -10,12 +13,51 @@ const EasyMode = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (isGameOn) {
+      intervalRef.current = setInterval(spawnTarget, 1000);
+    }
+
+    return () => clearInterval(intervalRef.current);
+  }, [isGameOn]);
+
+  let target;
+
+  const containerRef = useRef();
+  const intervalRef = useRef();
+
+  const startGame = () => {
+    setIsGameOn(true);
+    setIsTargetOn(true);
+  };
+
+  const spawnTarget = () => {
+    const containerHeight = containerRef.current.offsetHeight;
+    const containerWidth = containerRef.current.offsetWidth;
+
+    const randomTop = Math.random() * (containerHeight - 150);
+    const randomLeft = Math.random() * (containerWidth - 150);
+
+    setTargetPosition({ top: randomTop + "px", left: randomLeft + "px" });
+
+    setIsTargetOn(true);
+  };
+
+  const hitTarget = () => {
+    setIsTargetOn(false);
+  };
+
   return (
     <main>
       <header>
         <Link href="/">
           <a className="logo">Aim Training</a>
         </Link>
+        {!isGameOn && (
+          <button className="start-btn" onClick={startGame}>
+            Start Game
+          </button>
+        )}
       </header>
       <img
         style={cursorStyle}
@@ -23,6 +65,17 @@ const EasyMode = () => {
         src="/static/icons/grab.png"
         alt="grab cursor"
       />
+      <div ref={containerRef} className="game-container">
+        {isTargetOn && (
+          <img
+            style={targetPosition}
+            src="/static/icons/ethereum.png"
+            alt="target"
+            className="target"
+            onClick={hitTarget}
+          />
+        )}
+      </div>
     </main>
   );
 };
